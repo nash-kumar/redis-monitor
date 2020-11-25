2e2efa45113f8ca3d09a6f6ee8614b8e607165d3
nash-kumar <nareshkumar.bn@accionlabs.com>
2020-11-25 16:21:05 +0530

    Rendering View

--- index.js
+++ index.js
@@ -1,5 +1,7 @@
-const app = require('express')();
+const express = require("express");
+const app = express();
 const bodyParser = require('body-parser');
+const path = require("path");
 require('dotenv').config();
 const port = process.env.PORT || 8080;
 
@@ -16,7 +18,8 @@ models.sequelize.sync().then(() => {
 app.use('/redis', monitorRoute);
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => res.send('Initial route'))
+app.get('/', (req, res) => res.sendFile(path.join(__dirname, "public", "index.html")))
 
 app.listen(port, () => console.log("App listening on", port))
